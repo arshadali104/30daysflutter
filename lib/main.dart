599c4bf43f2a9a3a1538423bb84696c8fccaebae
arshad <arshad_ali104@gmail.com>
2021-11-27 19:13:55 -0800

    Day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/home_page.dart';
+import 'package:flutter_application_1/Pages/home_page.dart';
+import 'package:flutter_application_1/Pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,7 +11,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     
     return MaterialApp(
-      home: HomePage(),
+      //home: HomePage(),
+      routes: {
+     "/":(context)=> LoginPage(),
+     "/login":(context) => LoginPage(),
+     "/home":(context) => HomePage()
+    },
     );
-}
+ }
 }
