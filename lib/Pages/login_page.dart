051c00dd0b878fe5cbc10d9650fb73e85f59f5db
arshad <arshad_ali104@gmail.com>
2021-12-03 18:46:29 -0800

    Day-4 completed

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -5,8 +5,49 @@ class  LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text("Login Page"),
+      color: Colors.white,
+      child: Column(
+        children: [
+          Image.asset("assets/images/login_iamge.png",
+          fit:BoxFit.cover,),
+          SizedBox(
+            height: 20.0,
+            ),
+            
+          Text(
+            "welcome",
+            style: TextStyle(
+            fontSize: 24,
+            fontWeight:FontWeight.bold,
+          ),
+          ),
+          SizedBox(
+            height: 20.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              child: Column(children: [
+                TextFormField(decoration:
+              InputDecoration(hintText: "Enter username",
+              labelText: "username",
+              ),
+              ),
+              TextFormField(
+                obscureText: true,
+                decoration:
+              InputDecoration(hintText: "Enter password",
+              labelText: "password",
+              ),
+              ),
+              ElevatedButton(
+                onPressed: (){
+                  print("Hi code pur");
+                }, 
+                child: Text("Login")
+              ),                
+              ],),
+            )
+        ],
         ),
       );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/Pages/home_page.dart';
 import 'package:flutter_application_1/Pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,6 +12,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     
     return MaterialApp(
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily,
+        primaryTextTheme: GoogleFonts.latoTextTheme()
+
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      initialRoute: "/",
       //home: HomePage(),
       routes: {
      "/":(context)=> LoginPage(),
